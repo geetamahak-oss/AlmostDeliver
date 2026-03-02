d1b9416e89a5052d6e0a45260b6558276bc815c3
devansh sahani <geetamahak@gmai.com>
2026-03-02 23:18:19 +0530

    Fixed index view and added cart logic

--- shop/views.py
+++ shop/views.py
@@ -1,54 +1,32 @@
-from django.shortcuts import render, redirect
-from .models import Product, Order
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product  # पक्का करें कि आपके मॉडल का नाम Product ही है
+from .cart import Cart
 
-def home(request):
-    query = request.GET.get('search')
-    category = request.GET.get('cat') # यहाँ हम कैटेगरी का नाम पकड़ रहे हैं
-    
+# 1. होमपेज (जो एरर दे रहा था)
+def index(request):
     products = Product.objects.all()
-
-    # अगर सर्च किया गया है
-    if query:
-        products = products.filter(name__icontains=query)
-    
-    # अगर कैटेगरी पर क्लिक किया गया है
-    if category:
-        products = products.filter(category=category)
-        
     return render(request, 'shop/index.html', {'products': products})
 
-def place_order(request, product_id):
-    product = Product.objects.get(id=product_id)
-    
-    if request.method == "POST":
-        # फॉर्म से डेटा निकालना
-        name = request.POST.get('customer_name')
-        addr = request.POST.get('address')
-        ph = request.POST.get('phone')
-        
-        # डेटाबेस में ऑर्डर सेव करना
-        Order.objects.create(
-            product=product,
-            customer_name=name,
-            address=addr,
-            phone=ph
-        )
-        # पुराना return render हटाकर यह नया वाला डालें:
-        return render(request, 'shop/success.html', {
-            'customer_name': name,
-            'product_name': product.name
-        })
-    
-    # अगर पहली बार पेज खोल रहे हैं (GET request), तो फॉर्म दिखाओ
-    return render(request, 'shop/order_form.html', {'product': product})
-    from .cart import Cart
+# 2. सामान की पूरी जानकारी
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'shop/product_detail.html', {'product': product})
 
+# 3. कार्ट में सामान जोड़ना
 def add_to_cart(request, product_id):
     cart = Cart(request)
     cart.add(product_id)
     return redirect('cart_detail')
 
+# 4. कार्ट का पेज देखना
 def cart_detail(request):
+    cart_obj = Cart(request)
+    # यहाँ हम कार्ट के अंदर के डेटा को संभालते हैं
+    return render(request, 'shop/cart.html', {'cart': cart_obj})
+
+# 5. कार्ट से सामान हटाना (Optional पर काम आएगा)
+def item_clear(request, id):
     cart = Cart(request)
-    # यहाँ हम कार्ट के प्रोडक्ट्स को डेटाबेस से निकालेंगे दिखाने के लिए
-    return render(request, 'shop/cart.html', {'cart': cart})
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
